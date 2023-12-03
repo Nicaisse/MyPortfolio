b0359c36dcc20e4085992fa14340cdbe5c8fb295
bastian2012 <bjeanritchy@gmail.com>
2023-12-03 07:36:49 -0500

    compression des images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+
+site: 'https://astronaut.github.io'
+base: 'https://github.com/bastian2012/portfolio'
 export default defineConfig({});
