e50ade6f60c6e735c3931cc742f95bb572e160c6
Nicaisse <bryannicaisse2001@gmail.com>
2025-10-03 15:40:09 -0400

    Refactor site and base configuration to utilize environment variables in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import rehypeExternalLinks from 'rehype-external-links';
 
+// Utilise les variables d'environnement (Astro charge automatiquement .env)
+const base = process.env.PUBLIC_BASE || '/';
+const site = process.env.PUBLIC_SITE || 'http://localhost:4321';
+
 export default defineConfig({
-  site: 'https://nicaisse.github.io/portfolio',
-  base: '/portfolio',
+  site,
+  base,
   integrations: [react()],
   markdown: {
     rehypePlugins: [
